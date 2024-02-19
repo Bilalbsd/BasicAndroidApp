294cf184c290093bb18a8a8ac855abdaa042a6af
Bilal <dev.bilalb@gmail.com>
2024-02-19 16:17:14 +0100

    Add confirmation activity + layout

---
+++ app/src/main/java/com/example/basicandroidapp/ConfirmActivity.kt
@@ -0,0 +1,45 @@
+package com.example.basicandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class ConfirmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm)
+
+        val lastnameTextView = findViewById<TextView>(R.id.lastname_field)
+        val firstnameTextView = findViewById<TextView>(R.id.firstname_field)
+        val ageTextView = findViewById<TextView>(R.id.age_field)
+        val domainTextView = findViewById<TextView>(R.id.domain_field)
+        val phoneNumberTextView = findViewById<TextView>(R.id.phoneNumber_field)
+        val confirmButton = findViewById<Button>(R.id.ok_button)
+        val returnButton = findViewById<Button>(R.id.back_button)
+
+        // Récupération des données saisies depuis l'intent
+        val lastname = intent.getStringExtra("lastname")
+        val firstname = intent.getStringExtra("firstname")
+        val age = intent.getStringExtra("age")
+        val domain = intent.getStringExtra("domain")
+        val phoneNumber = intent.getStringExtra("phoneNumber")
+
+        // Affichage des données saisies dans les TextView
+        lastnameTextView.text = getString(R.string.label_lastname) + " $lastname"
+        firstnameTextView.text = getString(R.string.label_firstname) +" $firstname"
+        ageTextView.text = getString(R.string.label_age) +" $age"
+        domainTextView.text = getString(R.string.label_domain) +" $domain"
+        phoneNumberTextView.text = getString(R.string.label_phoneNumber) +" $phoneNumber"
+
+        // Gestion du bouton OK pour lancer l'intent implicite : Appel téléphonique
+        confirmButton.setOnClickListener {
+
+        }
+
+        // Gestion du bouton Retour pour revenir à l'activité précédente
+        returnButton.setOnClickListener {
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/example/basicandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/basicandroidapp/MainActivity.kt
@@ -2,10 +2,65 @@ package com.example.basicandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.app.AlertDialog
+import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val validateBtn = findViewById<Button>(R.id.validateButton)
+        validateBtn.setOnClickListener {
+            onValidate()
+        }
+
+        val returnBtn = findViewById<Button>(R.id.returnButton)
+        returnBtn.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun onValidate() {
+        // Initialisation des EditText de activity_main
+        val lastname = findViewById<EditText>(R.id.lastnameEditText)
+        val firstname = findViewById<EditText>(R.id.firstnameEditText)
+        val age = findViewById<EditText>(R.id.ageEditText)
+        val domain = findViewById<EditText>(R.id.domainEditText)
+        val phoneNumber = findViewById<EditText>(R.id.phoneNumberEditText)
+
+        if (lastname.text.trim().toString().isEmpty() || firstname.text.trim().toString().isEmpty()) {
+            Toast.makeText(this, getString(R.string.name_error), Toast.LENGTH_SHORT).show()
+        }
+        else if (phoneNumber.text.trim().toString().isEmpty()) {
+            Toast.makeText(this, getString(R.string.phoneNumber_error), Toast.LENGTH_SHORT).show()
+        } else {
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            alertDialogBuilder.setTitle(getString(R.string.title_validation))
+            alertDialogBuilder.setMessage(getString(R.string.message_validation))
+            alertDialogBuilder.setCancelable(false)
+
+            alertDialogBuilder.setPositiveButton(getString(R.string.alert_yes)) { _, _ ->
+                val intent = Intent(this, ConfirmActivity::class.java)
+
+                intent.putExtra("lastname", lastname.text.toString())
+                intent.putExtra("firstname", firstname.text.toString())
+                intent.putExtra("age", age.text.toString())
+                intent.putExtra("domain", domain.text.toString())
+                intent.putExtra("phoneNumber", phoneNumber.text.toString())
+
+                startActivity(intent)
+            }
+
+            alertDialogBuilder.setNegativeButton(getString(R.string.alert_no)) { _, _ ->
+                Toast.makeText(this, getString(R.string.cancelled_validation), Toast.LENGTH_SHORT).show()
+            }
+
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
+        }
     }
 }
